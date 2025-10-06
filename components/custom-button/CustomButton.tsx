a68301c359c4bf4ad92d8cbaac4b2c329c16f4a5
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-10-06 18:29:41 -0300

    update CustomButton - component's props using interface

--- components/custom-button/CustomButton.tsx
+++ components/custom-button/CustomButton.tsx
@@ -1,10 +1,17 @@
 import type { IconType } from 'react-icons';
 import './customButton.css';
 import Button from 'react-bootstrap/Button';
+import type { MouseEventHandler } from 'react';
 
-const CustomButton = ({ name, Icon }: { name: String; Icon: IconType }) => {
+interface ButtonProps {
+  name: String;
+  Icon: IconType;
+  onHandleClick: MouseEventHandler;
+}
+
+const CustomButton = ({ name, Icon, onHandleClick }: ButtonProps) => {
   return (
-    <Button size='lg'>
+    <Button size='lg' onClick={onHandleClick}>
       <span style={{ padding: '0.5rem' }}>{<Icon />}</span>
       <span style={{ padding: '1px solid red' }}>{name}</span>
     </Button>
--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,29 @@ import { SiPuppeteer } from 'react-icons/si';
 import CustomButton from '../components/custom-button/CustomButton';
 import { useState } from 'react';
 
+async function handleFetch() {
+  console.log('FETCH DATA!');
+}
+async function handleClear() {
+  console.log('CLEAR DATA!');
+}
+
 function App() {
   const [countries, setCountries] = useState([]);
   return (
     <>
       <h1>MERN + Puppeteer</h1>
       <div className='button-container section'>
-        <CustomButton name='Scrap with Puppeteer' Icon={SiPuppeteer} />
-        <CustomButton name='Delete BD' Icon={BsDatabaseFillX} />
+        <CustomButton
+          name='Scrap with Puppeteer'
+          Icon={SiPuppeteer}
+          onHandleClick={handleFetch}
+        />
+        <CustomButton
+          name='Delete BD'
+          Icon={BsDatabaseFillX}
+          onHandleClick={handleClear}
+        />
       </div>
       <div className='table-container section'>
         <DataTable data={countries}></DataTable>
